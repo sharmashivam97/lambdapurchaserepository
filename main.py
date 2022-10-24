f142d340fd8e26ec2b213a0a5de9d95a8ff1787f
sharmashivam97 <sharmashivam97@gmail.com>
2022-10-24 09:04:44 -0600

    MyCommit

---
+++ main.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+
+def handler(event, context):
+    sns_client = boto3.client("sns")
+    sns_arn ="arn:aws:sns:ca-central-1:700366089459:fanout" 
+    print(event)
+
+    for record in event["Records"]:
+        product = json.loads(record["body"])
+        print(product)
+
+    try:
+        sns_client.publish(
+            TopicArn=sns_arn,
+            Subject=f"Your Order {product['id']} was placed",
+            Message=record["body"]
+            
+            )
+
+        print("successfully sent notification")
+    except Exception as exp:
+        print(f"error occured, {exp}")
